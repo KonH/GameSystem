8a37252f198d756f27c42da7ad7758001b689b9c
KonH <konh@yandex.ru>
2020-02-08 14:26:05 +0700

    Setup first build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,40 +1,30 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.exec
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2019.2"
 
 project {
-
-    buildType(Build)
+    subProject {
+        name = "ResourceMonitor"
+        id = RelativeId(name)
+        buildType(DeployResourceMonitor)
+    }
 }
 
-object Build : BuildType({
-    name = "Build"
+object DeployResourceMonitor : BuildType({
+    name = "Deploy"
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        exec {
+            name = "Deploy"
+            path = "nuke"
+            arguments = "--target DeployDotNet --targetProject ResourceMonitor --targetRuntime linux-arm --selfContained true --localPiHome %env.LOCAL_PI_DIRECTORY%"
+        }
     }
 
     triggers {
