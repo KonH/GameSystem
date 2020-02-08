3320469d6a2365cb5c3b7fb18b344c0955554cb3
KonH <konh@yandex.ru>
2020-02-09 00:17:56 +0700

    Clicker game console client

--- Clicker.ConsoleClient/Program.cs
+++ Clicker.ConsoleClient/Program.cs
@@ -1,9 +1,43 @@
-﻿using System;
+﻿using Clicker.Common;
+using Clicker.Common.Config;
+using Clicker.Common.State;
+using Core.Common.Command;
+using Core.Common.Utils;
+using Core.ConsoleClient;
 
 namespace Clicker.ConsoleClient {
 	class Program {
-		static void Main(string[] args) {
-			Console.WriteLine("Hello World!");
+		static void Main() {
+			GameState createState() => new GameState();
+			var commands = TypeResolver.GetSubclasses<ICommand<GameConfig, GameState>>(typeof(GameConfig).Assembly);
+			var config = CreateConfig();
+			var client = new StandaloneConsoleClient<GameConfig, GameState>(
+				commands, new CommandQueue(), config, createState);
+			client.Run();
+		}
+
+		static GameConfig CreateConfig() {
+			return new GameConfig {
+				Resource = new ResourceConfig {
+					ResourceByClick = 10
+				},
+				Upgrade = new UpgradeConfig {
+					Levels = new[] {
+						new UpgradeLevel {
+							Cost = 30,
+							Power = 2.0
+						},
+						new UpgradeLevel {
+							Cost = 100,
+							Power = 3.0
+						},
+						new UpgradeLevel {
+							Cost = 200,
+							Power = 5.0
+						}
+					}
+				}
+			};
 		}
 	}
 }
