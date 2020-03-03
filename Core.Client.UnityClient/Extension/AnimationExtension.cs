cbe999b5f695371dad7be431aa2ee13fb96401eb
KonH <konh@yandex.ru>
2020-03-03 23:26:24 +0700

    Async animation extension

---
+++ Core.Client.UnityClient/Extension/AnimationExtension.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Core.Client.UnityClient.Threading;
+using UnityEngine;
+
+namespace Core.Client.UnityClient.Extension {
+	public static class AnimationExtension {
+		public static async Task Wait(this Animation anim) {
+			anim.Play();
+			while ( true ) {
+				if ( !anim.isPlaying ) {
+					break;
+				}
+				await Async.WaitForUpdate;
+			}
+		}
+	}
+}
