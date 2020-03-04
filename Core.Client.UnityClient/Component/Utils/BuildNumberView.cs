8f87100442ac9c5163ba7ae1b1e89d775ae60f94
KonH <konh@yandex.ru>
2020-03-04 22:33:53 +0700

    Refactor components

--- Core.Client.UnityClient/Component/Utils/BuildNumberView.cs
+++ Core.Client.UnityClient/Component/Utils/BuildNumberView.cs
@@ -1,9 +1,9 @@
 using TMPro;
 using UnityEngine;
 
-namespace Core.Client.UnityClient.Components.TMP {
+namespace Core.Client.UnityClient.Component.Utils {
 	[RequireComponent(typeof(TMP_Text))]
-	public sealed class TmpBuildNumberView : MonoBehaviour {
+	public sealed class BuildNumberView : MonoBehaviour {
 		void Start() {
 			var text = GetComponent<TMP_Text>();
 			text.text = Application.version;
