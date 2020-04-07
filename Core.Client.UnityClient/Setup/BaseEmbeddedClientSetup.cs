37e512d1758d8c270fa04683e2f1b0768aabc0b1
konh <konh@yandex.ru>
2020-04-07 19:26:16 +0700

    Refactor: move common dependencies into BaseEmbeddedClientSetup

---
+++ Core.Client.UnityClient/Setup/BaseEmbeddedClientSetup.cs
@@ -0,0 +1,46 @@
+using Core.Client.Shared;
+using Core.Client.UnityClient.DependencyInjection;
+using Core.Client.UnityClient.Shared;
+using Core.Common.Config;
+using Core.Common.State;
+using Core.Service.Extension;
+using Core.Service.Repository;
+using Core.Service.Repository.Config;
+using Core.Service.Repository.State;
+using Core.Service.UseCase.GetConfig;
+using Core.Service.UseCase.GetState;
+using Core.Service.UseCase.UpdateState;
+
+namespace Core.Client.UnityClient.Setup {
+	abstract class BaseEmbeddedClientSetup<TConfig, TState> : CommonClientSetup<TConfig, TState>
+		where TConfig : class, IConfig where TState : class, IState, new() {
+
+		public override void Configure(ServiceProvider provider) {
+			base.Configure(provider);
+
+			provider.AddService(JsonRepositoryDecoratorSettings.Create<TConfig>());
+			provider.AddService<IConfigRepository<TConfig>, InMemoryConfigRepository<TConfig>>();
+
+			provider.AddService(JsonRepositoryDecoratorSettings.Create<TState>());
+			provider.AddService<IStateRepository<TState>, InMemoryStateRepository<TState>>();
+
+			var config = provider.GetService<TConfig>();
+			provider.AddService(new GetSingleConfigStrategy<TConfig>.Settings(config.Version));
+			provider.AddService<IGetConfigStrategy<TConfig>, GetSingleConfigStrategy<TConfig>>();
+			provider.AddService<GetConfigUseCase<TConfig>>();
+
+			provider.AddService<GetStateUseCase<TState>>();
+
+			provider.AddService<UpdateStateUseCase<TConfig, TState>>();
+
+			provider.AddService(new StateFactory<TState>(() => new TState()));
+
+			provider.GetService<IConfigRepository<TConfig>>()
+				.Add(config);
+
+			provider.AddService<ISettingsSource, PersistentSettingsSource>();
+			provider.AddService<UserIdGenerator>();
+			provider.AddService<UserIdSource>();
+		}
+	}
+}
