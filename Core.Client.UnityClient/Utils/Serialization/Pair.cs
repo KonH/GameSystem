c52a47f746f3b726bbf21a9ddeb780bf42c58547
konh <konh@yandex.ru>
2020-04-12 16:13:33 +0700

    Feature: base classes for dictionary-like Unity serialization

---
+++ Core.Client.UnityClient/Utils/Serialization/Pair.cs
@@ -0,0 +1,6 @@
+namespace Core.Client.UnityClient.Utils.Serialization {
+	public abstract class Pair<TKey, TValue> {
+		public TKey   Key;
+		public TValue Value;
+	}
+}
---
+++ Core.Client.UnityClient/Utils/Serialization/StrGoPair.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+namespace Core.Client.UnityClient.Utils.Serialization {
+	[Serializable]
+	public sealed class StrGoPair : Pair<string, GameObject> {}
+}
