2ea6c683eab89eaf125012aa51e8888d9ca4082b
konh <konh@yandex.ru>
2020-04-12 16:18:10 +0700

    Idler: connection & command error windows with scene reload

--- IdlerUnityClient/Assets/Scripts/IdlerRunner.cs
+++ IdlerUnityClient/Assets/Scripts/IdlerRunner.cs
@@ -1,12 +1,15 @@
+using System.Threading.Tasks;
 using Idler.Common.Config;
 using Idler.Common.State;
 using Idler.UnityClient.View;
 using Core.Client.Shared;
 using Core.Client.UnityClient;
 using Core.Client.UnityClient.DependencyInjection;
+using Core.Client.UnityClient.Window;
 using Core.Common.CommandExecution;
 using Idler.UnityClient.Reaction;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Idler.UnityClient {
 	public sealed class IdlerRunner : UnityRunner<GameConfig, GameState> {
@@ -15,12 +18,19 @@ namespace Idler.UnityClient {
 		[Header("Resources")]
 		[SerializeField] ResourceView _resourceView = null;
 
+		[Header("Windows")]
+		[SerializeField] WindowSettings _windowSettings = null;
+
+		WindowManager _windowManager = null;
+
 		void OnValidate() {
 			Debug.Assert(_stateView, nameof(_stateView));
 			Debug.Assert(_resourceView, nameof(_resourceView));
+			Debug.Assert(_windowSettings, nameof(_windowSettings));
 		}
 
 		async void Awake() {
+			_windowManager = new WindowManager(_windowSettings);
 			AddReactions();
 			DisableStateView();
 			await Initialize();
@@ -32,7 +42,7 @@ namespace Idler.UnityClient {
 			executor.AddReaction(new AddResourceCommandReaction(_resourceView));
 		}
 
-		protected override void HandleInitialization(InitializationResult result) {
+		protected override async Task HandleInitialization(InitializationResult result) {
 			switch ( result ) {
 				case InitializationResult.Ok _: {
 					IsReadyForCommand = true;
@@ -43,14 +53,22 @@ namespace Idler.UnityClient {
 
 				case InitializationResult.Error error: {
 					Debug.LogError(error.Description);
+					await _windowManager.Show<MessageWindow>(
+						nameof(MessageWindow),
+						w => w.Show("Server Error", "Failed to connect to server", "Retry"));
+					SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 					break;
 				}
 			}
 		}
 
-		protected override void HandleCommandResult(CommandApplyResult result) {
+		protected override async Task HandleCommandResult(CommandApplyResult result) {
 			if ( result is CommandApplyResult.Error error ) {
 				Debug.LogError(error.Description);
+				await _windowManager.Show<MessageWindow>(
+					nameof(MessageWindow),
+					w => w.Show("Command Error", "Error happens while communication with server", "Retry"));
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 			}
 		}
 
