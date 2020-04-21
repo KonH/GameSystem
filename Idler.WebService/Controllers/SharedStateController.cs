1a52b235a73f3b0760514f8b7e0ed8c95db49a91
konh <konh@yandex.ru>
2020-04-21 22:19:59 +0700

    Idler: get shared state from service

---
+++ Idler.WebService/Controllers/SharedStateController.cs
@@ -0,0 +1,25 @@
+using Core.Common.Utils;
+using Core.Service.WebService.Shared;
+using Idler.Common.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Idler.WebService.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public sealed class SharedStateController : ControllerBase {
+		readonly ILogger<SharedStateController> _logger;
+		readonly SharedStateRepository          _repository;
+
+		public SharedStateController(ILoggerFactory loggerFactory, SharedStateRepository repository) {
+			_logger     = loggerFactory.Create<SharedStateController>();
+			_repository = repository;
+		}
+
+		[HttpGet]
+		public WebResponse Get() {
+			var state = _repository.Get();
+			_logger.LogTrace($"Return shared resources: '{state.Resources}'.");
+			return new WebResponse(state);
+		}
+	}
+}
